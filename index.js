84dfa5d0e08c88d229a0dd147ae872f92f94f6b7
nishidatomoki <86286013+nishidatomoki@users.noreply.github.com>
2022-01-12 15:15:54 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require("http");
+
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+  var url = "public" + (req.url.endsWith("/") ? req.url + "index.html" : req.url);
+  if (fs.existsSync(url)) {
+    fs.readFile(url, (err, data) => {
+      if (!err) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+      }
+    });
+  }
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
